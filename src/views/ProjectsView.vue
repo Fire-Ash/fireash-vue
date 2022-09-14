fb584455e003972c8defe0819b4ee3e3c9fa4172
Fire-Ash <101067949+Fire-Ash@users.noreply.github.com>
2022-09-13 20:07:48 -0400

    update projects to new domain ig lol

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,20 +1,10 @@
 <template>
     <div class="space-y-10">
-        <ProjectInfo title="LVForum"
-                     description="LVForum is a forum I'm working on that uses Laravel. It's kind of just being worked on in the background, as I have no motivation for it, but I will slowly be releasing updates to make it featured enough for people to actually use it. LVForum is MIT licenced."
-                     screenshot="/images/projects/LVForum01.png"
-                     :links="[
-                         {
-                             name: 'Source Code',
-                             link: 'https://github.com/Fire-Ash/lvforum',
-                             icon: 'fa-brands fa-git-alt'
-                         },
-                         {
-                             name: 'Demo Instance',
-                             link: 'https://lvforum.fireash.cf',
-                             icon: 'fa-solid fa-file'
-                         }
-                     ]"
+        <ProjectInfo v-for="project in projects"
+                     :title="project.title"
+                     :description="project.description"
+                     :screenshot="project.screenshot"
+                     :links="project.links"
         />
     </div>
 </template>
@@ -24,6 +14,29 @@ import ProjectInfo from "@/components/ProjectInfo.vue";
 
 export default {
     name: "ProjectsView",
-    components: { ProjectInfo }
+    components: { ProjectInfo },
+    data() {
+        return {
+            projects: [
+                {
+                    title: 'LVForum',
+                    description: 'LVForum is a forum I\'m working on that uses Laravel. It\'s kind of just being worked on in the background, as I have no motivation for it, but I will slowly be releasing updates to make it featured enough for people to actually use it. LVForum is MIT licenced.',
+                    screenshot: '/images/projects/LVForum01.png',
+                    links: [
+                        {
+                            name: 'Source Code',
+                            link: 'https://github.com/Fire-Ash/lvforum',
+                            icon: 'fa-brands fa-git-alt'
+                        },
+                        {
+                            name: 'Demo Instance',
+                            link: 'https://lvforum.fireash.xyz',
+                            icon: 'fa-solid fa-file'
+                        }
+                    ]
+                }
+            ]
+        }
+    }
 };
 </script>
