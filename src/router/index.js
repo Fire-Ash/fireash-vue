3439315478271ec9a775b8a99b28976378761aa2
Ash <101067949+Fire-Ash@users.noreply.github.com>
2022-10-03 18:56:39 -0400

    add nprogress, split /projectsroute, split css

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
-import ProjectsView from "@/views/ProjectsView.vue";
 import NotFound from "@/views/NotFound.vue";
+import nProgress from "nprogress";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,32 +10,35 @@ const router = createRouter({
         {
             path: "/",
             name: "home",
-            component: HomeView
+            component: HomeView,
         },
         {
             path: "/about",
             name: "about",
-            component: AboutView
+            component: AboutView,
         },
         {
             path: "/projects",
             name: "projects",
-            component: ProjectsView
+            component: () => import("@/views/ProjectsView.vue"),
         },
         {
-            path: '/:pathMatch(.*)*',
-            name: 'NotFound',
-            component: NotFound
+            path: "/:pathMatch(.*)*",
+            name: "NotFound",
+            component: NotFound,
         },
-        // {
-        //     path: "/about",
-        //     name: "about",
-        //     // route level code-splitting
-        //     // this generates a separate chunk (About.[hash].js) for this route
-        //     // which is lazy-loaded when the route is visited.
-        //     component: () => import("../views/AboutView.vue")
-        // }
-    ]
+    ],
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.name) {
+        nProgress.start();
+    }
+    next();
+});
+
+router.afterEach(() => {
+    nProgress.done();
 });
 
 export default router;
