8f80a3f9159f7b4ac6a59379242d947b5a9b113d
Ash <101067949+Fire-Ash@users.noreply.github.com>
2022-10-09 16:22:00 -0400

    change BackToTop compnent to use composition api

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,33 +1,29 @@
-<script>
-    export default {
-        name: "BackToTop",
-        mounted() {
-            window.addEventListener('scroll', this.handleScroll)
-        },
-        methods: {
-            handleScroll: function () {
-                if (document.documentElement.scrollTop > 20) {
-                    document.getElementById('topBtn').style.display = 'block'
-                } else {
-                    document.getElementById('topBtn').style.display = 'none'
-                }
-            },
-            backToTop: function () {
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
+<script setup>
+    import { onMounted } from 'vue'
+
+    onMounted(() => {
+        window.addEventListener('scroll', handleScroll)
+    })
+
+    function handleScroll() {
+        if (document.documentElement.scrollTop > 20) {
+            document.getElementById('topBtn').style.display = 'block'
+        } else {
+            document.getElementById('topBtn').style.display = 'none'
         }
+    }
+
+    function toTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
     }
 </script>
 
 <template>
-    <button class="fixed bottom-5 right-5 btn rounded-full"
-            style="height:50px;width:50px;display:none;"
-            @click="backToTop"
+    <button class="fixed bottom-5 right-5 btn btn-square rounded-full"
+            style="display:none;"
+            @click="toTop"
             id="topBtn"
     >
-        <i class="fa-solid fa-angle-up text-xl"></i>
+        <i class="fa-solid fa-angle-up fa-xl"></i>
     </button>
 </template>
